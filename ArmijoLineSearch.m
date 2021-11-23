2001c5654a6c8a15d3ad989b71d49d85ae816b7a
NathanaelSix <32201433+NathanaelSix@users.noreply.github.com>
2021-11-23 11:15:55 +0100

    Update ArmijoLineSearch.m

--- ArmijoLineSearch.m
+++ ArmijoLineSearch.m
@@ -1,12 +1,16 @@
 function alpha = ArmijoLineSearch(objHandle,objVal,x,dx,dir, options)
-% ARMIJOLINESEARCH runs a simple backtracking line search with armijo
-% conditions
+% ARMIJOLINESEARCH runs a simple backtracking line search with Armijo
+% condition
 % INPUT
 %   objHandle : function handle to the objective function
 %   objVal : objective function value in x
 %   x : current point 
 %   dx : current gradient in x
 %   dir : search direction in x, usually -dx
+%   options : optional input struct to change the default parameters of the line search
+%           - options.alpha_start: starting step size (default: 1)
+%           - options.reduction_factor: factor with which alpha will be multiplied in each iteration when the Armijo condition is not yet met (default: 1/2)
+%           - options.c: the control parameter of the Armijo condition, c in ]0, 1[ (default: 1e-4)
 
 
 if nargin == 5
